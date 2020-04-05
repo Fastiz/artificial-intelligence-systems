1d6ebf0c30db1a7a38325e9cc2dda429b4146f35
fastiz <fastiz@itba.edu.ar>
2020-04-04 22:26:53 -0300

    Fixed IDDFS.java

--- sokoban-solver/src/back/algorithms/IDDFS.java
+++ sokoban-solver/src/back/algorithms/IDDFS.java
@@ -8,14 +8,21 @@ import java.util.*;
 
 public class IDDFS implements Algorithm {
 
+    private HashSet<Game> hashSet;
+
+    private boolean remainingToSearch;
+
+    private int visitedNodes;
     private int expandedNodes;
     private Game gameSolved;
     private int maxDepth;
 
-    IDDFS(int maxDepth) {
-        if (maxDepth < 0)
-            maxDepth = 1;
-        this.maxDepth = maxDepth;
+    public IDDFS() {
+        hashSet = new HashSet<>();
+
+        this.remainingToSearch = true;
+
+        this.maxDepth = Integer.MAX_VALUE;
     }
 
     public int getMaxDepth() {
@@ -33,39 +40,38 @@ public class IDDFS implements Algorithm {
         expandedNodes = 0;
         boolean result = false;
 
-        Queue<Game> gamesToAnalize = new LinkedList<>();
-        gamesToAnalize.add(game);
-        Queue<Game> nextIterationGames = new LinkedList<>();
 
         long startTime = System.nanoTime();
 
         /* En gameToAnalize pongo los que voy a analizar, en nextIterationGames pongo los que se pasan de depth cuando hago dfs,
         entonces cuando me retorna, en la proxima iteracion, los que van a analizar son los que se pasaron.
          */
-        for (int i = 0; i < maxDepth & !result; i++) {
-            while (!gamesToAnalize.isEmpty()) {
-                Game gameToAnalize = gamesToAnalize.poll();
-                result = recursiveIDDFS(gameToAnalize, nextIterationGames, i);
-            }
-            gamesToAnalize.addAll(nextIterationGames);
-            nextIterationGames.clear();
+        for (int i = 0; i < maxDepth && this.remainingToSearch && !result; i++) {
+            this.expandedNodes = 0;
+            this.remainingToSearch = false;
+            result = recursiveIDDFS(game, i);
+            hashSet.clear();
         }
 
         long endTime = System.nanoTime();
 
         AlgorithmSolution solution;
         if (result)
-            solution = new AlgorithmSolution(this.expandedNodes, nextIterationGames.size(), this.gameSolved, endTime - startTime);
+            solution = new AlgorithmSolution(this.expandedNodes, 1, this.gameSolved, endTime - startTime);
         else
             solution = new AlgorithmSolution(false, this.expandedNodes, endTime - startTime);
 
         return solution;
     }
 
-    private boolean recursiveIDDFS(Game game, Queue<Game> nextIterationGames, int depthLeft) {
+    private boolean recursiveIDDFS(Game game, int depthLeft) {
+        this.visitedNodes++;
+        this.hashSet.add(game);
+
+        List<Game> children = game.calculateChildrenWithStack();
 
         if (depthLeft < 0) {
-            nextIterationGames.add(game);
+            this.remainingToSearch = this.remainingToSearch || !children.isEmpty();
             return false;
         }
 
@@ -74,13 +80,15 @@ public class IDDFS implements Algorithm {
             return true;
         }
 
-        List<Game> children = game.calculateChildrenWithStack();
         for (int i = 0; i < children.size(); i++) {
             Game gameChild = children.get(i);
-            if (recursiveIDDFS(gameChild, nextIterationGames, depthLeft - 1)) {
-                if (i + 1 == children.size())
-                    this.expandedNodes++;
-                return true;
+
+            if(!this.hashSet.contains(gameChild)){
+                if (recursiveIDDFS(gameChild, depthLeft - 1)) {
+                    if (i + 1 == children.size())
+                        this.expandedNodes++;
+                    return true;
+                }
             }
         }
 
