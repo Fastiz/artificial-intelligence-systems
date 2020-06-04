5acb437aac110893d75de016bc5904b2edff2560
fastiz <fastiz@itba.edu.ar>
2020-06-04 14:52:34 -0300

    now neighborhood is computed using distance between weights instead of coordinates

--- unsupervised-learning/src/kohonen/Simulation.java
+++ unsupervised-learning/src/kohonen/Simulation.java
@@ -4,6 +4,7 @@ import utils.CsvReader;
 import utils.Vector;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
@@ -12,7 +13,7 @@ public class Simulation {
     public static void run(){
 
         //Kohonen map generator
-        int latticeDim = 5;
+        int latticeDim = 3;
         int inputDim = 7;
         KohonenMap map = (new KohonenMap.Builder())
                 .setLattice(new SquareLattice(cellGenerator(inputDim), latticeDim))
@@ -37,15 +38,36 @@ public class Simulation {
             return;
         }
 
+
         //Training
-        for(int t=0; t<10000; t++){
+        System.out.println("\nStarting training");
+        for(int t=0; t< 30000; t++){
             for(Vector vector : categories.values()){
                 map.step(vector);
             }
         }
 
         //Done training
-        System.out.println("Done training!");
+        System.out.println("\nDone training!");
+
+        //Creating a matrix to classify input values into the matrix coordinates
+        FrequencyMatrix frequencyMatrix = new FrequencyMatrix(latticeDim);
+        for(String key : categories.keySet()){
+            Vector vector = categories.get(key);
+
+            Lattice.Coord coord = map.activate(vector);
+            frequencyMatrix.addMember(coord.getI(), coord.getJ(), key);
+        }
+        
+        //Print frequency of categories inside each cell
+        System.out.println("\nNumber of members on each cell:");
+        for(int i=0; i<latticeDim; i++){
+            for(int j=0; j<latticeDim; j++){
+                System.out.print(frequencyMatrix.getCellPopulation(i, j) + "");
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
 
     }
 
