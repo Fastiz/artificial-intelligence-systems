0515f64ba3f1824806a23f7a7be36caa9a12a26c
Miguel <mdiluca@itba.edu.ar>
2020-06-28 23:05:22 -0300

    ruido

---
+++ autoencoder/java/src/Noise.java
@@ -0,0 +1,35 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Noise {
+
+    public static List<Double> saltAndPepper(List<Double> list, double noiseFactor) {
+        return applyNoise(list, noiseFactor, false);
+    }
+
+    public static List<Double> gaussianNoise(List<Double> list, double noiseFactor) {
+        return applyNoise(list, noiseFactor, true);
+    }
+
+    private static List<Double> applyNoise(List<Double> list, double noiseFactor, boolean gaussian) {
+        List<Double> noiseList = new ArrayList<>();
+        Random random = new Random();
+        for (Double bit : list) {
+            //No estoy tan seguro si esta bien que la meia sea 0 en guassiano
+            double randomNumber = gaussian ? random.nextGaussian() : random.nextDouble();
+            if (randomNumber < noiseFactor)  {
+                noiseList.add(invertedBit(bit));
+            } else {
+                noiseList.add(bit);
+            }
+        }
+        return noiseList;
+    }
+
+    private static double invertedBit(double bit) {
+        return bit == 0 ? 1 : 0;
+    }
+}
