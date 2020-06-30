5d8330db3487ef746ff5c8d7c23f0196e2413910
Miguel <mdiluca@itba.edu.ar>
2020-06-29 23:37:28 -0300

    error, generate random

--- autoencoder/java/src/Main.java
+++ autoencoder/java/src/Main.java
@@ -16,29 +16,30 @@ import java.util.stream.IntStream;
 public class Main {
 
     public static void main(String[] args){
-        encodeDecodeForFont(2, 0);
+        encodeDecodeForFont(2, 0, -1);
     }
 
-    private static void encodeDecodeForFont(int fontNum, double noiseFactor){
+    private static void encodeDecodeForFont(int fontNum, double noiseFactor, int limit) {
 
-        List<LetterData> trainingData = FontManager.getFont(fontNum, noiseFactor);
+        List<LetterData> trainingData = FontManager.getFont(fontNum, limit);
         AutoEncoder autoEncoder = getEncoder(trainingData.get(0).getInput().size(), Arrays.asList(10,2,10));
 
 
         stochasticTraining(autoEncoder, trainingData, 200000);
 
-
         try(BufferedWriter bf = new BufferedWriter(new FileWriter("data"))){
             double totalError = 0;
             String[] fontNames = FontManager.getFontNames(fontNum);
 
-
+            double error = 0;
             for (int i=0; i<trainingData.size(); i++){
                 LetterData letterData = trainingData.get(i);
                 List<Double> c = letterData.getInput();
 
                 List<Double> encoded = autoEncoder.encode(c);
                 List<Double> encodeDecode = autoEncoder.decode(encoded);
+
+                error += getECM(c, encodeDecode);
 
                 List<Double> encodeDecodeRounded = encodeDecode.stream()
                         .mapToDouble(v->v>0?1.0:-1.0).boxed().collect(Collectors.toList());
@@ -65,6 +66,9 @@ public class Main {
                 }
             }
 
+            error /= trainingData.size();
+            System.out.println("ERROR: " + error);
+
             System.out.println(String.format("Average difference is %s out of %s (%s)",
                     totalError/trainingData.size(),
                     trainingData.get(0).getInput().size(),
@@ -73,10 +77,41 @@ public class Main {
             System.err.print(e);
             return;
         }
+    }
+
+    private static double getECM(List<Double> original, List<Double> decoded) {
+        double error = 0;
+        for(int i = 0; i < original.size(); i++) {
+            error += Math.sqrt(Math.pow(original.get(i) - decoded.get(i), 2));
+        }
+        return error;
+    }
+
+    private static void generateFont(int fontNum, int limit){
+        List<LetterData> trainingData = FontManager.getFont(fontNum, limit);
+        AutoEncoder autoEncoder = getEncoder(trainingData.get(0).getInput().size(), Arrays.asList(10,2,10));
 
 
+        stochasticTraining(autoEncoder, trainingData, 200000);
 
+        List<Double> encodedMean = new ArrayList<>(2);
+        encodedMean.add(0.0);
+        encodedMean.add(0.0);
+        for (LetterData letterData : trainingData) {
+            printLetter(letterData.getInput());
+            List<Double> encoded = autoEncoder.encode(letterData.getInput());
+            System.out.println('\n');
+            encodedMean.set(0, encodedMean.get(0) + encoded.get(0));
+            encodedMean.set(1, encodedMean.get(1) + encoded.get(1));
+        }
 
+        encodedMean.set(0, encodedMean.get(0) / trainingData.size());
+        encodedMean.set(1, encodedMean.get(1) / trainingData.size());
+
+        List<Double> encodeDecodeRounded2 = autoEncoder.decode(encodedMean).stream()
+                .mapToDouble(v->v>0?1.0:-1.0).boxed().collect(Collectors.toList());
+
+        printLetter(encodeDecodeRounded2);
     }
 
     private static void stochasticTraining(AutoEncoder ae, List<LetterData> trainingData, int itNum){
